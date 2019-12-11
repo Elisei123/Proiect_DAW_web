473f9f0da2f80cdd92f702a82d1d7886d81da574
Elisei123 <43387542+Elisei123@users.noreply.github.com>
2019-12-11 23:03:05 +0200

    Create a To-DO and Database tasks

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include "base.php"?>
+<?php include "includes/conectare.php"?>
 <?php
 if(isset($_SESSION['id'])) {
     echo "<title>Salut " . ucfirst($_SESSION['username']) . "</title>";
@@ -13,6 +14,22 @@ if(isset($_SESSION['id'])) {
         ';
     }
     ?>
-    <br><h3 style="text-align: center; margin-top: 100px"> Base TO-DO list </h3>
+    <br><h3 style="text-align: center; margin-top: 26px"> Base TO-DO list </h3>
+    <form method="post" action="index.php">
+        <input type="text" name="task" class="task_input">
+        <button type="submit" class="task_submit" name="submit">Submit</button>
+    </form>
+
+    <?php
+    if(isset($_POST['submit'])) {
+        $task = $_POST['task'];
+        $date = date("h:i:sa") . " " . date("d-m-Y");
+        $Username = intval($_SESSION['id']);
+
+    $sql = "INSERT INTO tasks (Task, Data_curenta, ID_USER) VALUES ('$task', '$date', '$Username')";
+    $result = mysqli_query($conectare, $sql);
+    header("Location: ../Proiect_DAW_web/index.php");
+    }
+    ?>
 </div>
 
